e75ccd4002344d2aa8ef6fb2f55c67c9599769d8
bj293 <bj293@Brandon-PC>
2020-05-12 17:25:55 -0500

    Added City and Attraction classes with the methods described in the UML

---
+++ src/City.java
@@ -0,0 +1,31 @@
+
+public class City {
+	String name;
+	double area;
+	double population;
+	HashMap<String, Attraction> attractions = new HashMap<String, Attraction>;
+	
+	public City(String name, double area, double population) {
+		this.name = name;
+		this.area = area;
+		this.population = population;
+	}
+	
+	public void addAttraction(Attraction newAttraction) {
+		newAttractionName = newAttraction.getName();
+		attractions.put(newAttractionName, newAttraction);
+	}
+	
+	public HashMap<String, Attractions> getAttractions(){
+		return attractions;
+	}
+	
+	public double getArea() {
+		return area;
+	}
+	
+	public double getPopulation() {
+		return population;
+	}
+
+}
