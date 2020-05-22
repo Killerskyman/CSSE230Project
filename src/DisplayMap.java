970b37326c0fa5cc61c0710b9499f62110538cb9
Keagan <finkenka@rose-hulman.edu>
2020-05-21 19:21:21 -0500

    Fix error in DisplayMap

--- src/DisplayMap.java
+++ src/DisplayMap.java
@@ -36,7 +36,7 @@ public class DisplayMap extends JPanel {
     }
     
     public void colorRoute(Collection<City> route){
-        City[] routeArr = route.toArray(City[]::new);
+    	City[] routeArr = (City[]) route.toArray(new City[route.size()]);
         for(int i = 0; i < routeArr.length-1; i++) {
             City start = routeArr[i];
             City end = routeArr[i+1];
